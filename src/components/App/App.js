cf5e2d865d33b9ddf9d40c8245993cacc2d794dc
Clevis Necol Valentin Willrich <Hotness@The-Good-Mac.local>
2018-01-26 03:38:22 -1000

    Added functionality to save a playlist to a user's account

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -50,6 +50,7 @@ class App extends React.Component {
       this.addTrack = this.addTrack.bind(this);
       this.removeTrack = this.removeTrack.bind(this);
       this.updatePlaylistName = this.updatePlaylistName.bind(this)
+      this.savePlaylist = this.savePlaylist.bind(this);
   }
 
   addTrack(track){
@@ -68,6 +69,10 @@ class App extends React.Component {
     this.setState({playlistName: name});
   }
 
+  savePlaylist(){
+    let trackURIs = this.state.playlistTracks.map(track => track.uri);
+  }
+
   render() {
     return (
       <div>
@@ -76,7 +81,7 @@ class App extends React.Component {
           <!-- Add a SearchBar component -->
           <div className="App-playlist">
             <SearchResults searchResults={this.state.searchResults} onAdd={this.addTrack}/>
-            <Playlist playlistName={this.state.playlistName} playlistTracks={this.state.playlistTracks}  onRemove={this.removeTrack} onNameChange={this.updatePlaylistName}/>
+            <Playlist playlistName={this.state.playlistName} playlistTracks={this.state.playlistTracks}  onRemove={this.removeTrack} onNameChange={this.updatePlaylistName} onSave={this.savePlaylist}/>
           </div>
         </div>
       </div>
