de2e2a4151e41255dac6b9dec5b54dfb686055fc
Clevis Necol Valentin Willrich <Hotness@The-Good-Mac.hawaii.rr.com>
2018-01-27 02:37:49 -1000

    Hooked up SearchBar to Spotify search, touched Spotify.js

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -4,6 +4,16 @@ import './SarchBar.css';
 class SearchBar extends React.Component {
   constructor(props){
     super(props);
+    this.search = this.search.bind(this);
+    this.handleTermChange = this.handleTermChange.bind(this);
+  }
+
+  search(){
+    this.props.onSearch(this.state.term);
+  }
+
+  handleTermChange(event){
+    this.setState({term: event.target.value});
   }
 
   render(){
