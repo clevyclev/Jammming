6fce83e0d1c9cbd4c42bddeb64df3e9ae12800bb
Clevis Necol Valentin Willrich <Hotness@The-Good-Mac.hawaii.rr.com>
2018-01-27 02:38:22 -1000

    Updated SearchBar-to-Spotify-search hookup

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -19,7 +19,7 @@ class SearchBar extends React.Component {
   render(){
     return (
       <div className="SearchBar">
-        <input placeholder="Enter A Song, Album, or Artist" />
+        <input placeholder="Enter A Song, Album, or Artist" onChange={this.handleTermChange}/>
         <a>SEARCH</a>
       </div>
     )
